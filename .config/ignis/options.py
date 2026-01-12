dfb04e0b0b322ae10e9a3562f20371bd5989043f
NonExistPlayer <nonexistplayer@gmail.com>
2026-01-12 15:35:30 +0300

    shell: Fixed options path

--- .config/ignis/options.py
+++ .config/ignis/options.py
@@ -1,9 +1,10 @@
+import os
 from ignis.options_manager import OptionsManager, OptionsGroup
 
 
 class Options(OptionsManager):
     def __init__(self):
-        super().__init__(file="~/.config/ignis/options")
+        super().__init__(file=os.path.expanduser("~/.config/ignis/options"))
 
     class Clock(OptionsGroup):
         showseconds: bool = False
