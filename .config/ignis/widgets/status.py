8400439192f262972c606b8260e817b936fa1b8b
NonExistPlayer <nonexistplayer@gmail.com>
2026-02-14 12:49:03 +0300

    shell: Added keyboard layout

--- .config/ignis/widgets/status.py
+++ .config/ignis/widgets/status.py
@@ -1,8 +1,10 @@
 from ignis.options import options
 
-from ignis.widgets import Box, Icon, Button
+from ignis.widgets import Box, Icon, Label, Button
 from ignis.window_manager import WindowManager
+from ignis.services.niri import NiriService
 from ignis.services.network import NetworkService
+from ignis.services.hyprland import HyprlandService
 from ignis.services.notifications import NotificationService
 
 
@@ -13,6 +15,33 @@ class StatusBar(Button):
         netservice = NetworkService.get_default()
         winmanager = WindowManager.get_default()
         nservice = NotificationService.get_default()
+        hyprservice = HyprlandService.get_default()
+        niriservice = NiriService.get_default()
+
+        keylayout = Label()
+
+        if hyprservice.is_available:
+            keyboard = hyprservice.main_keyboard
+
+            # if the keylayouts less than 2, then hide
+            if len(keyboard.layout.split(",")) < 2:
+                keylayout.set_visible(False)
+            else:
+                keylayout.set_label(keyboard.bind(
+                    "active_keymap", lambda k: k[0:2]))
+                keylayout.set_tooltip_text(keyboard.bind("active_keymap"))
+        elif niriservice.is_available:
+            keylayouts = niriservice.keyboard_layouts
+
+            # if the keylayouts less than 2, then hide
+            if len(keylayouts.names) < 2:
+                keylayout.set_visible(False)
+            else:
+                keylayout.set_label(keylayouts.bind(
+                    "current_name"), lambda k: k[0:2])
+                keylayout.set_tooltip_text(keylayouts.bind("current_name"))
+        else:
+            keylayout.set_visible(False)
 
         ethernet_icon = Icon(
             image=netservice.ethernet.bind("icon_name"),
@@ -46,6 +75,7 @@ class StatusBar(Button):
         box = Box(
             spacing=15,
             child=[
+                keylayout,
                 Icon(image=netservice.wifi.bind("icon_name")),
                 ethernet_icon,
                 vpn_icon,
