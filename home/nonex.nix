fa354ce69d79a34d7c5acbe52f1b898a8b489e10
NonExistPlayer <nonexistplayer@gmail.com>
2026-06-10 20:12:41 +0300

    feat(home): add and configure kitty

--- home/nonex.nix
+++ home/nonex.nix
@@ -18,5 +18,18 @@
     };
   };
 
+  programs.kitty = {
+    enable = true;
+
+    settings = {
+      window_padding_width = 2.8;
+
+      cursor_shape = "beam";
+      cursor_trail = 1;
+
+      confirm_os_window_close = 0;
+    };
+  };
+
   home.stateVersion = "26.05";
 }
