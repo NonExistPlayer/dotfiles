d97c5e0695989fd23f43aad1f8f19d7ff06f17ff
NonExistPlayer <nonexistplayer@gmail.com>
2026-06-10 19:41:28 +0300

    feat(home): configure git

--- home/nonex.nix
+++ home/nonex.nix
@@ -4,5 +4,19 @@
   home.username = "nonex";
   home.homeDirectory = "/home/nonex";
 
+  programs.git = {
+    enable = true;
+
+    settings = {
+      user.name = "NonExistPlayer";
+      user.email = "nonexistplayer@gmail.com";
+
+      alias = {
+        s = "status";
+        l = "log --oneline";
+      };
+    };
+  };
+
   home.stateVersion = "26.05";
 }
