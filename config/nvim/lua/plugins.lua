31b860a0ba5afa1620bd979a21ce12549d6d1154
NonExistPlayer <nonexistplayer@gmail.com>
2026-06-12 14:22:44 +0300

    feat(nvim): add cool `oxocarbon` theme

--- config/nvim/lua/plugins.lua
+++ config/nvim/lua/plugins.lua
@@ -1,1 +1,2 @@
 require("plugins.neo-tree")
+require("plugins.oxocarbon")
---
+++ config/nvim/lua/plugins/oxocarbon.lua
@@ -0,0 +1,4 @@
+vim.pack.add({ 'https://github.com/nyoom-engineering/oxocarbon.nvim' })
+
+vim.opt.background = "dark"
+vim.cmd("colorscheme oxocarbon")
